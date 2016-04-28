7e18fc56753c5b527380f6ead77504eb3b6f9643
The_only <626396834@qq.com>
2016-04-28 11:14:41 +0800

    创建了新项目

---
+++ GitCodeProj/GitCodeProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitCodeProj
+//
+//  Created by The_only on 16/4/28.
+//  Copyright © 2016年 zhangning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitCodeProj/GitCodeProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCodeProj
+//
+//  Created by The_only on 16/4/28.
+//  Copyright © 2016年 zhangning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
